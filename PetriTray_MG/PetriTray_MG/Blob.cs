082adbbc435e5aa6a4bd0f86aa948da22b7f887a
Dodddo2 <37241511+Dodddo2@users.noreply.github.com>
2018-11-19 18:09:34 +0100

    Blobs

---
+++ PetriTray_MG/PetriTray_MG/Blob.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetriTray_MG
+{
+    class Blob
+    {
+        public EntitySprite sprite;
+        public Vector3 Pivot;
+        public Effect Effect;
+        public List<Models.Metaball> metaballs = new List<Models.Metaball>();
+
+        public Blob(Vector3 pos, GraphicsDevice device, Effect effect)
+        {
+            int width = 600;
+            int height = 600;
+            sprite = new EntitySprite(device);
+            Pivot = pos;
+            Effect = effect;
+            sprite.Sprite = new Texture2D(device, width, height);
+            sprite.WorldPos = new Vector3(Pivot.X-(width/2), Pivot.Y-(height/2), Pivot.Z);
+        }
+
+        public void AddBall(Models.Metaball ball)
+        {
+            ball.SpritePos = ball.Position - Camera.Main.Position;
+            metaballs.Add(ball);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for(int i = 0; i < metaballs.Count; i++)
+            {
+                
+                Effect.Parameters["positions"].Elements[i].SetValue(metaballs[i].SpritePos);
+                Effect.Parameters["colors"].Elements[i].SetValue(metaballs[i].RGBA);
+                Effect.Parameters["parameters"].Elements[i].SetValue(metaballs[i].Parameters);
+                
+                spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+                Effect.CurrentTechnique.Passes[0].Apply();
+                spriteBatch.Draw(sprite.Sprite, new Vector2(0, 0), Color.White);
+                spriteBatch.End();
+            }
+
+        }
+
+    }
+}
---
+++ PetriTray_MG/PetriTray_MG/Content/Metaball.fx
@@ -0,0 +1,27 @@
+﻿#define MAXBALLS 20
+
+float3 positions[MAXBALLS];
+float4 colors[MAXBALLS];
+float4 parameters[MAXBALLS];
+
+float4 MetaballFalloff(float2 coords : TEXCOORD0, float counter) : COLOR1
+{
+	float distance = sqrt(pow(coords.x - positions[counter].x, 2) + pow(coords.y - positions[counter].y, 2));
+	if (distance < parameters[counter].x) {
+		return colors[counter];
+	}
+	return float4(0,0,0,0);
+}
+
+float4 PS(float2 coords: TEXCOORD0) : COLOR0
+{
+	return MetaballFalloff(coords, 0);
+}
+
+technique Ball
+{
+	pass P0
+	{
+		PixelShader = compile ps_4_0_level_9_3 PS();
+	}
+}
