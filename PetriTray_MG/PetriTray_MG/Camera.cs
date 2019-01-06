a250bc5878bb7e267bbaa25a2fecac996b0e65df
Dodddo2 <37241511+Dodddo2@users.noreply.github.com>
2019-01-07 00:30:23 +0100

    Fair Metaballs, ver.1

--- PetriTray_MG/PetriTray_MG/Camera.cs
+++ PetriTray_MG/PetriTray_MG/Camera.cs
@@ -9,6 +9,7 @@ namespace PetriTray_MG
 {
     class Camera
     {
+
         public Vector3 Position = new Vector3(0, 0, 10);
         public Vector3 Direction = Vector3.Normalize(new Vector3(0, 0, -1));
         public Vector3 Up = Vector3.Up;
@@ -17,12 +18,19 @@ namespace PetriTray_MG
         //public Matrix Projection => Matrix.CreatePerspectiveFieldOfView(MathHelper.Pi/3, 1.0f, 0.5f, 100.0f);
 
         public Rectangle Resolution = new Rectangle(0, 0, 800, 600);
-        public float CameraSensitivity = 0.5f;
+        public float CameraSensitivity = 0.02f;
 
         public static readonly Camera Main = new Camera();
 
+        public Camera()
+        {
+            Console.WriteLine("Camera");
+        }
+
+
         public void Move(Vector3 moveTo)
         {
+            Console.WriteLine(Position.ToString());
             Position += moveTo * CameraSensitivity;
         }
     }
--- PetriTray_MG/PetriTray_MG/Content/Metaball.fx
+++ PetriTray_MG/PetriTray_MG/Content/Metaball.fx
@@ -5,13 +5,25 @@ float heats[20];
 float4 MetaballFalloff(float2 coord: TEXCOORD1, float counter) : COLOR1
 {
 	float dist = distance(coord, positions[counter]);
-	float falloff = exp(-((dist*dist) / (2 * heats[counter] * heats[counter])));
+	float falloff = exp(-(pow(dist, 4) / (2 * heats[counter] * heats[counter])));
 	return (colors[counter] * falloff);
 }
 
 float4 PS(float2 coords: TEXCOORD0) : COLOR0
 {
+	float treshold = 0.2f;
+	
 	float4 ret= MetaballFalloff(coords, 0) + MetaballFalloff(coords, 1);
+	ret = ret - treshold;
+	ret = lerp(0, 1/(1 - treshold), ret);
+	float blobStencil = ret.a;
+	float alphaStencil = -ret.a + 1;
+	ret.a = (ret.a < 0.4f ? 0 : ret.a);
+	
+	
+
+
+
 	return ret;
 }
 
